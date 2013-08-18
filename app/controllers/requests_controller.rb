0ca0bdab59f82a143bcccdd47e376cf16b51e2bd
XEdP3X <mario@xedp3x.de>
2013-08-18 15:11:53 +0200

    filter request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,10 +1,17 @@
 class RequestsController < ApplicationController
   before_action :set_request, only: [:show, :edit, :update, :destroy]
-
   # GET /requests
   # GET /requests.json
   def index
-    @requests = Request.all
+    if params[:state] then
+      if params[:state] != '' then
+        @requests = Request.where(state: params[:state]).order :request_number
+      else
+        @requests = Request.all.order :request_number
+      end
+    else
+      @requests = Request.all.order :request_number
+    end
   end
 
   # GET /requests/1
@@ -12,29 +19,8 @@ class RequestsController < ApplicationController
   def show
   end
 
-  # GET /requests/new
-  def new
-    @request = Request.new
-  end
-
   # GET /requests/1/edit
   def edit
-  end
-
-  # POST /requests
-  # POST /requests.json
-  def create
-    @request = Request.new(request_params)
-
-    respond_to do |format|
-      if @request.save
-        format.html { redirect_to @request, notice: 'Request was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @request }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @request.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /requests/1
@@ -62,13 +48,14 @@ class RequestsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_request
-      @request = Request.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def request_params
-      params.require(:request).permit(:request_number, :note, :has_data, :state)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_request
+    @request = Request.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def request_params
+    params.require(:request).permit(:request_number, :note, :has_data, :state)
+  end
 end
--- db/migrate/20130817130326_create_requests.rb
+++ db/migrate/20130817130326_create_requests.rb
@@ -3,7 +3,7 @@ class CreateRequests < ActiveRecord::Migration
     create_table :requests do |t|
       t.text :request_number
       t.text :note
-      t.boolean :has_data
+      t.boolean :has_data, default: false
       t.text :state
 
       t.timestamps
