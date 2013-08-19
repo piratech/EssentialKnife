c5dd91d2f9e6c601c5307f7721c39125d860eea2
XEdP3X <mario@xedp3x.de>
2013-08-19 21:40:43 +0200

    fix scan read

--- app/models/scan.rb
+++ app/models/scan.rb
@@ -26,46 +26,49 @@ class Scan < ActiveRecord::Base
     note= ''
 
     begin
-      Hash.from_xml(xml)["barcodes"]["source"]["index"]["symbol"].each do | code |
-        if code["type"] == "QR-Code" then
-          puts "QR-Code gefunden"
+      h= Hash.from_xml(xml)["barcodes"]["source"]["index"]
+      if !h.nil? then
+        h= h["symbol"]
+        h= [h] if !h.kind_of?(Array)
+        h.each do | code |
 
-          if data.nil? then
-            data= JSON.parse(code["data"]).to_hash
-          else
-            data= false
-            note+= "\nzuviele QR-Codes gefunden!"
+          if code["type"] == "QR-Code" then
+            puts "QR-Code gefunden"
+
+            if data.nil? then
+              data= JSON.parse(code["data"]).to_hash
+            else
+              data= false
+              note+= "\nzuviele QR-Codes gefunden!"
+            end
+
+            puts JSON.pretty_generate(data)
           end
 
-          puts JSON.pretty_generate(data)
-        end
+          if code["type"] == "EAN-8" then
+            if request_number.nil? then
+              request_number= code["data"]
+              request_number = false if !EAN8.valid? request_number
+            else
+              request_number= false
+              note+= "\zuviele Track-Codes gefunden!"
+            end
 
-        if code["type"] == "EAN-8" then
-          if request_number.nil? then
-            request_number= code["data"]
-            request_number = false if !EAN8.valid? request_number
-          else
-            request_number= false
-            note+= "\zuviele Track-Codes gefunden!"
+            puts "Track-Code #{request_number} gefunden"
           end
-
-          puts "Track-Code #{request_number} gefunden"
         end
       end
 
       if request_number then
         request= Request.find_or_create_by(request_number: request_number)
-        if data then
-          if !request.has_data then
-          request.read_data data
-          else
-            request.state = 'Problem'
-            request.note  = "Mehere QR-Codes gefunden"
-          end
-        request.save
+
+        if !request.has_data then
+        request.read_data data
         else
-          request= nil
+          request.state = 'Problem'
+          request.note  = "Mehere QR-Codes gefunden"
         end
+      request.save
       else
         request= nil
       end
@@ -73,6 +76,8 @@ class Scan < ActiveRecord::Base
     rescue
       note = "Parsing error!"
     end
+
+    request_number = nil if request_number == false
 
     scan = self.create data: data, request_number: request_number, barcode: xml, note: note, request: request
 
